36b0b8d00eeab112273564c643409ae73e58ea77
D14LB3L0 <diegobdev2024@gmail.com>
2025-07-05 22:07:33 +0100

    infinite scroll

--- lib/screens/listview_builder_screen.dart
+++ lib/screens/listview_builder_screen.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
 
-class ListviewBuilderScreen extends StatelessWidget {
-   
+class ListviewBuilderScreen extends StatefulWidget {
   const ListviewBuilderScreen({super.key});
-  
+
+  @override
+  State<ListviewBuilderScreen> createState() => _ListviewBuilderScreenState();
+}
+
+class _ListviewBuilderScreenState extends State<ListviewBuilderScreen> {
+  final List<int> imagesIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    scrollController.addListener(() {
+      if ((scrollController.position.pixels + 500) >=
+          scrollController.position.maxScrollExtent) {
+            add5();
+          }
+    });
+  }
+
+  void add5() {
+    final lastId = imagesIds.last;
+    imagesIds.addAll([1, 2, 3, 4, 5].map((e) => lastId + e));
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,18 +37,22 @@ class ListviewBuilderScreen extends StatelessWidget {
         removeTop: true,
         removeBottom: true,
         child: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (BuildContext context, int index){
+          physics: const BouncingScrollPhysics(),
+          controller: scrollController,
+          itemCount: imagesIds.length,
+          itemBuilder: (BuildContext context, int index) {
             return FadeInImage(
               width: double.infinity,
               height: 300,
               fit: BoxFit.cover,
               placeholder: const AssetImage('assets/jar-loading.gif'),
-              image: NetworkImage('https://picsum.photos/500/300?image${index + 1}'),
+              image: NetworkImage(
+                'https://picsum.photos/500/300?image${imagesIds[index]}',
+              ),
             );
           },
         ),
-      )
+      ),
     );
   }
 }
