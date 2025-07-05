da7e0876f5d8e69dcd748f4c34284b68b1c83c7a
D14LB3L0 <diegobdev2024@gmail.com>
2025-07-05 08:35:19 +0100

    added listTitles

--- lib/screens/list_view1_screen.dart
+++ lib/screens/list_view1_screen.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
 
 class Listview1Screen extends StatelessWidget {
-   
+  final options = const [
+    'Megaman',
+    'Metal Gear',
+    'Super Smash',
+    'Final Fantasy',
+  ];
+
   const Listview1Screen({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Listview type 1"),
-      ),
+      appBar: AppBar(title: const Text("Listview type 1")),
       body: Center(
-         child: ListView(
-          children: const[
-            Text('Hello world'),
-            Text('Hello world')
+        child: ListView(
+          children: [
+            ...options.map(
+              (game) => ListTile(
+                title: Text(game),
+                trailing: const Icon(Icons.arrow_forward_ios_outlined),
+              ),
+            )
           ],
-         )
+        ),
       ),
     );
   }
